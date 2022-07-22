60b51e633ab79abeecdd1df5e62e2de043abe640
poker <wanglei311600@qq.com>
2022-07-22 13:06:21 +0800

    set DEBUG=False

--- recruitment/recruitment/settings/local.py
+++ recruitment/recruitment/settings/local.py
@@ -1,5 +1,7 @@
 from recruitment.settings.base import *
 
+DEBUG = False
+
 ALLOWED_HOSTS = ["*"]
 
 DINGTALK_WEB_HOOK = "https://oapi.dingtalk.com/robot/send?access_token=8fdfb07566eb88cf9a162fed379b4d54ea1678f09fe274d34e6a4ea7f65312c4"
